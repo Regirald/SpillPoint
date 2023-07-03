6f547b9218a1d7ea4976ec3cd0fae6fc7256546b
regirald <leblond.r83@gmail.com>
2023-07-03 11:09:49 +0200

    optimisation propagation

--- Layer_Propagation.cpp
+++ Layer_Propagation.cpp
@@ -6,11 +6,21 @@
 
 using namespace std;
 
+void PrintQ( queue<pair<int,int>> q){
+        if(q.empty()){
+            cout<<"liste vide!"<<endl;
+        }
+    while (!q.empty()) {
+        cout << "(" << q.front().first << ", " << q.front().second << ") ";
+        q.pop();
+    }
+    cout << endl;
+}
 
-list<pair<int,int>> propagateByLayer(vector<vector<int>>& grid, int startX, int startY,vector<vector<pair<bool, int>>> & visited ,int h) {
+
+list<pair<int,int>>  propagateByLayer(vector<vector<int>>& grid, vector<vector<pair<bool, int>>> & visited ,queue<pair<int, int>> &q,int h) {
     int numRows = grid.size();
     int numCols = grid[0].size();
-    int val_init=grid[startX][startY];
 
 
     // Définir les directions possibles (haut, droite, bas, gauche)
@@ -18,13 +28,13 @@ list<pair<int,int>> propagateByLayer(vector<vector<int>>& grid, int startX, int 
     vector<int> dy = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
 
     // Créer une file d'attente pour la propagation par couche
-    queue<pair<int, int>> q;
     list<pair<int,int>> List_sp;
-    
+    queue<pair<int, int>> qNextLevel;
     vector<vector<bool>> visited_sp(numRows, vector<bool>(numCols, false));
+    vector<vector<bool>> visited_nl(numRows, vector<bool>(numCols, false));
     // Ajouter le point de départ à la file d'attente et le marquer comme visité
-    q.push({startX, startY});
-    visited[startX][startY] = {true,0};
+    
+    visited[q.front().first][q.front().second] = {true,0};
     int Layer_depth=1;
 
     // Parcourir chaque couche jusqu'à ce que la file d'attente soit vide
@@ -52,9 +62,14 @@ list<pair<int,int>> propagateByLayer(vector<vector<int>>& grid, int startX, int 
                 // Spill Point detection
                 if (newX >= 0 && newX < numRows && newY >= 0 && newY < numCols && !visited[newX][newY].first && !visited_sp[newX][newY] && grid[newX][newY]<h) {
                         List_sp.push_back({newX, newY});
-                        visited_sp[newX][newY]=true;
+                        //visited_sp[newX][newY]=true;  to make stats overs outputs
                 }
-
+                 // Next level detection
+                if (newX >= 0 && newX < numRows && newY >= 0 && newY < numCols && !visited[newX][newY].first && !visited_nl[newX][newY] && grid[newX][newY]==h+1) {
+                        qNextLevel.push({newX, newY});
+                        visited_nl[newX][newY]=true;
+                        cout << "H+1 point (" << newX << ", " << newY << ") with value " << grid[newX][newY] << endl;
+                }
                 // Vérifier si les nouvelles coordonnées sont valides et non visitées
                 if (newX >= 0 && newX < numRows && newY >= 0 && newY < numCols && !visited[newX][newY].first && grid[newX][newY]==h) {
 
@@ -82,11 +97,16 @@ list<pair<int,int>> propagateByLayer(vector<vector<int>>& grid, int startX, int 
 
 
     } 
+    while (!qNextLevel.empty()) {
+        q.push(qNextLevel.front());
+        qNextLevel.pop();
+    }
     return List_sp;
 }
 
 
-void Raise_level(vector<vector<int>>& grid,vector<vector<pair<bool, int>>> &visited){
+
+void Raise_level(vector<vector<int>>& grid,vector<vector<pair<bool, int>>> &visited, int h){
     int rows = grid.size();
     int cols = grid[0].size();
 
@@ -94,7 +114,7 @@ void Raise_level(vector<vector<int>>& grid,vector<vector<pair<bool, int>>> &visi
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++)
             if (visited[i][j].first){
-                grid[i][j]++;
+                grid[i][j]=h;
             }
         }
     }
@@ -109,18 +129,29 @@ list<pair<int,int>> Find_Spill_Point(vector<vector<int>> grid, int startX, int s
 
     pair<int,int> coor_spill_pt={startX,startY};
     int h = grid[startX][startY];
+    queue<pair<int, int>> q;
+    q.push({startX, startY});
+    list<pair<int,int>> List_sp;
+    vector<vector<pair<bool, int>>> visited(rows, vector<pair<bool, int>>(cols, pair<bool, int>(false, 0)));
 
     while (h<10){
+        if (q.empty()){
+            q.push({startX, startY});
+        }
+        cout << "##########h =  "<<h<<endl;
 
-        cout << "##########################h =  "<<h<<endl;
-        vector<vector<pair<bool, int>>> visited(rows, vector<pair<bool, int>>(cols, pair<bool, int>(false, 0)));
-        list<pair<int,int>> List_sp= propagateByLayer(grid, startX, startY, visited, h);
+        list<pair<int,int>>List_sp= propagateByLayer(grid,  visited, q, h);
+        cout << "##################q =  "<<endl;
+        PrintQ(q);
+
         if (List_sp.size()!=0){
             return List_sp;
         }
+        
 
-        Raise_level(grid, visited);
+        Raise_level(grid, visited,h);
         h++;
+
 
 /*         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
@@ -134,9 +165,9 @@ list<pair<int,int>> Find_Spill_Point(vector<vector<int>> grid, int startX, int s
             }
             cout << endl;
         }  */
-       
-
     }
+
+
     list<pair<int,int>> List_null;
     return List_null;
 }
@@ -157,7 +188,7 @@ int main(){
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
-    {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},//10
+    {3, 2, 2, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},//10
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
     {3, 2, 2, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 2, 2, 3},
